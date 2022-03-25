95121ba8c29e7b5493db7e44415dac60f338407d
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-03-25 21:50:32 +0600

    bootstrap,fontawesome installed;favicon added;app name changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css'
+import { Button } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className='App'>
       <h1>Hello World</h1>
+      <Button variant='primary'>Primary</Button>
+      <FontAwesomeIcon icon={faCoffee} />
     </div>
   )
 }

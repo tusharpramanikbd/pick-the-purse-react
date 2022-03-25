dabe33b49ab2af84e552fee31fccc9b2576eca8d
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-03-26 00:34:56 +0600

    products section ui done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import './Product.css'
 import { Card } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
+  const { name, price, img } = props.product
   return (
     <Card className='product-card'>
-      <Card.Img variant='top' src={props.product.img} className='product-img' />
-      <Card.Body>
-        <Card.Title>{props.product.name}</Card.Title>
-        <Card.Text>Price: ${props.product.price}</Card.Text>
-      </Card.Body>
+      <Card.Img variant='top' src={img} className='product-img' />
+      <div className='product-body'>
+        <h5 className='product-title'>{name}</h5>
+        <Card.Text>Price: ${price}</Card.Text>
+      </div>
       <div className='text-center add-to-cart-btn'>
         <span>Add to Cart</span>
+        <FontAwesomeIcon icon={faCartPlus} className='cart-icon' />
       </div>
     </Card>
   )
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,7 +3,6 @@ import Product from '../Product/Product'
 import './Products.css'
 
 const Products = (props) => {
-  console.log(props)
   return (
     <div>
       <div className='product-container'>
